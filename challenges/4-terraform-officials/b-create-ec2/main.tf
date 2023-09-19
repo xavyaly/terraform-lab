83376f0b1b173109c35f6ebf2a4ff3cf8bfa541f
xavyaly <wellboy.alam13@gmail.com>
2023-09-19 12:36:34 +0530

    instance launch

--- challenges/4-terraform-officials/b-create-ec2/main.tf
+++ challenges/4-terraform-officials/b-create-ec2/main.tf
@@ -1,12 +1,3 @@
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-830c94e3"
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = "ExampleAppServerInstance"
-#   }
-# }
-
 resource "aws_instance" "web_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
